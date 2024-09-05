d7dba110d234c0c2e86d15bb232e50d3275d77bd
sbarl <74242154+sbarl@users.noreply.github.com>
2024-09-05 15:07:14 -0600

    Update main.py
    sarah put the function back in a normal place hopefully

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+def func():
+    print("hey, here's my change")
+
 def main():
     print("hello world!")
-    def func():
-        print("hey, here's my change")
+
 
 if __name__ == "__main__":
     main()
