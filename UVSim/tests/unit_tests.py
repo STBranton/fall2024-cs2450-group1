fc0d02ab07b3640989c839a2e932ff03a3cd7449
Kaden Kulkarni <kadenkulkarni@gmail.com>
2024-09-19 16:07:34 -0600

    Unit Testing

--- UVSim/tests/unit_tests.py
+++ UVSim/tests/unit_tests.py
@@ -1,12 +1,20 @@
 # Implement unit tests for each component (memory, CPU, instruction execution) to ensure functionality remains consistent.
 import unittest
-from UVSim.src.memory import Memory
+import os
+import sys
+current_dir = os.path.dirname(__file__)
+
+src_dir = os.path.abspath(os.path.join(current_dir, '../src'))
+sys.path.insert(0, src_dir)
+from memory import Memory # type: ignore
 
 class TestMemory(unittest.TestCase):
     def test_memory_store_and_retrieve(self):
         memory = Memory()
         memory.set_value(10, 1234)
         self.assertEqual(memory.get_value(10), 1234)
+        memory = Memory()
+        memory.getvalue(10,)
 
 if __name__ == "__main__":
     unittest.main()
