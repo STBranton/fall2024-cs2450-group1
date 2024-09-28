859363a1bff6578f7c190bbf5b3ee50ed369bed6
STBranton <Stuart@branton.co.uk>
2024-09-28 13:03:07 -0600

    CS2-19 Fix error in UVSim.py as mentioned in feedback

--- UVSim/src/UVSim.py
+++ UVSim/src/UVSim.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         accumulator = Accumulator()
         cpu = CPU(memory, accumulator)
 
-        while cpu.execute_instruction < 100:
+        while cpu.instruction_counter < 100:
             cpu.execute_instruction()
