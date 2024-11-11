8d6e0bdab9f4e7b4c543446d927fb2245dbfb862
STBranton <Stuart@branton.co.uk>
2024-11-11 16:47:54 -0700

    CS2-29 Increase test coverage

--- UVSim/tests/unit_tests.py
+++ UVSim/tests/unit_tests.py
@@ -4,7 +4,8 @@ import os
 import sys
 import unittest
 from pathlib import Path
-from unittest.mock import patch
+from unittest.async_case import IsolatedAsyncioTestCase
+from unittest.mock import patch, AsyncMock
 
 from UVSim.src.input_handler import CLIInputHandler
 
@@ -18,7 +19,7 @@ from accumulator import Accumulator  # type: ignore
 from cpu import CPU  # type: ignore
 
 
-class unitTests(unittest.TestCase):
+class unitTests(IsolatedAsyncioTestCase):
     def test_memory_store_and_retrieve1(self):
         memory = Memory(250)
         memory.set_value(10, 1234)
@@ -46,297 +47,352 @@ class unitTests(unittest.TestCase):
         program = load_program_from_file(file_path)
         cpu.memory.load_program(program)
 
-    async def test_read1(self):
+    def test_load_invalid_length_command(self):
+        file_path = Path(__file__).parent / "Test3.txt"
+        memory = Memory(250)
+        cpu = CPU(memory, input_handler=CLIInputHandler, output_callback=print)
+        program = load_program_from_file(file_path)
+        with self.assertRaises(ValueError) as context:
+            cpu.memory.load_program(program)
+        self.assertEqual(str(context.exception), "Program instructions must be either 4 or 6 digits (with optional '+' or '-' sign).")
+
+    async def test_read(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
-        address = 87
+        instruction = "010200"
+        memory.load_program([instruction])
+        address = 200
         valueGave = 5
         with patch('builtins.input', return_value=valueGave):
-            await cpu.handle_read(address)
+            await cpu.execute_instruction()
             newValue = cpu.memory.memory[address]
             self.assertEqual(newValue, valueGave)
 
-    async def test_read2(self):
+    async def test_invalid_read(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
-        address = 85
-        valueGave = 10
-        with patch('builtins.input', return_value=valueGave):
-            await cpu.handle_read(address)
-            newValue = cpu.memory.memory[address]
-            self.assertEqual(newValue, valueGave)
+        instruction = "010085"
+        memory.load_program([instruction])
+        return_value = "BOOM!"
+        with patch.object(input_handler, 'get_input', AsyncMock(return_value=return_value)):
+            with self.assertRaises(ValueError) as context:
+                await cpu.execute_instruction()
 
-    def test_write1(self):
+            self.assertEqual(str(context.exception), f"Invalid input '{return_value}'; expected an integer.")
+
+    async def test_write1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "011085"
+        memory.load_program([instruction])
         address = 85
         value = 1005
         cpu.memory.memory[address] = value
         captured_word = io.StringIO()
         with patch('sys.stdout', new=captured_word):
-            cpu.handle_write(address)
+            await cpu.execute_instruction()
         printed_value = int(captured_word.getvalue().replace("Output: ", "").strip())
         self.assertEqual(value, printed_value)
 
-    def test_write2(self):
+    async def test_write2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "011067"
+        memory.load_program([instruction])
         address = 67
         value = 9007
         cpu.memory.memory[address] = value
         captured_word = io.StringIO()
         with patch('sys.stdout', new=captured_word):
-            cpu.handle_write(address)
+            await cpu.execute_instruction()
         printed_value = int(captured_word.getvalue().replace("Output: ", "").strip())
         self.assertEqual(value, printed_value)
 
-    def test_load1(self):
+    async def test_load1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "020067"
+        memory.load_program([instruction])
         address = 67
         value = 567
         cpu.memory.memory[address] = value
-        cpu.handle_load(address)
+        await cpu.execute_instruction()
         self.assertEqual(cpu.accumulator.value, value)
 
-    def test_load2(self):
+    async def test_load2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "020045"
+        memory.load_program([instruction])
         address = 45
         value = 100085
         cpu.memory.memory[address] = value
-        cpu.handle_load(address)
+        await cpu.execute_instruction()
         self.assertEqual(cpu.accumulator.value, value)
 
-    def test_store1(self):
+    async def test_store1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "021045"
+        memory.load_program([instruction])
         address = 45
         original_value = 100085
         cpu.accumulator.value = original_value
-        cpu.handle_store(address)
+        await cpu.execute_instruction()
         new_value = cpu.memory.memory[address]
         self.assertEqual(new_value, original_value)
 
-    def test_store2(self):
+    async def test_store2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "021031"
+        memory.load_program([instruction])
         address = 31
         original_value = 10078
         cpu.accumulator.value = original_value
-        cpu.handle_store(address)
+        await cpu.execute_instruction()
         new_value = cpu.memory.memory[address]
         self.assertEqual(new_value, original_value)
 
-    def test_add1(self):
+    async def test_add1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "030031"
+        memory.load_program([instruction])
         address = 31
         original_accumulator_value = 10078
         original_address_value = 76
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_add(address)
+        await cpu.execute_instruction()
         values_added = original_address_value + original_accumulator_value
         self.assertEqual(values_added, cpu.accumulator.value)
 
-    def test_add2(self):
+    async def test_add2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "030056"
+        memory.load_program([instruction])
         address = 56
         original_accumulator_value = 10045
         original_address_value = 1234
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_add(address)
+        await cpu.execute_instruction()
         values_added = original_address_value + original_accumulator_value
         self.assertEqual(values_added, cpu.accumulator.value)
 
-    def test_subtract1(self):
+    async def test_subtract1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "031056"
+        memory.load_program([instruction])
         address = 56
         original_accumulator_value = 10045
         original_address_value = 1234
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_subtract(address)
+        await cpu.execute_instruction()
         values_subtracted = original_accumulator_value - original_address_value
         self.assertEqual(values_subtracted, cpu.accumulator.value)
 
-    def test_subtract2(self):
+    async def test_subtract2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "031087"
+        memory.load_program([instruction])
         address = 87
         original_accumulator_value = 132487
         original_address_value = 584
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_subtract(address)
+        await cpu.execute_instruction()
         values_subtracted = original_accumulator_value - original_address_value
         self.assertEqual(values_subtracted, cpu.accumulator.value)
 
-    def test_divide1(self):
+    async def test_divide1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "032056"
+        memory.load_program([instruction])
         address = 56
         original_accumulator_value = 10045
         original_address_value = 1234
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_divide(address)
-        values_divded = original_accumulator_value / original_address_value
-        self.assertEqual(values_divded, cpu.accumulator.value)
+        await cpu.execute_instruction()
+        values_divided = original_accumulator_value / original_address_value
+        self.assertEqual(values_divided, cpu.accumulator.value)
 
-    def test_divide2(self):
+    async def test_divide2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "032056"
+        memory.load_program([instruction])
         address = 56
         original_accumulator_value = 1000
         original_address_value = 10
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_divide(address)
-        values_divded = original_accumulator_value / original_address_value
-        self.assertEqual(values_divded, cpu.accumulator.value)
+        await cpu.execute_instruction()
+        values_divided = original_accumulator_value / original_address_value
+        self.assertEqual(values_divided, cpu.accumulator.value)
 
-    def test_multiply1(self):
+    async def test_multiply1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "033056"
+        memory.load_program([instruction])
         address = 56
         original_accumulator_value = 1000
         original_address_value = 10
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_multiply(address)
+        await cpu.execute_instruction()
         values_multiplied = original_accumulator_value * original_address_value
         self.assertEqual(values_multiplied, cpu.accumulator.value)
 
-    def test_multiply2(self):
+    async def test_multiply2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "033056"
+        memory.load_program([instruction])
         address = 56
         original_accumulator_value = 876
         original_address_value = 2
         cpu.memory.memory[address] = original_address_value
         cpu.accumulator.value = original_accumulator_value
-        cpu.handle_multiply(address)
+        await cpu.execute_instruction()
         values_multiplied = original_accumulator_value * original_address_value
         self.assertEqual(values_multiplied, cpu.accumulator.value)
 
-    def test_branch1(self):
+    async def test_branch1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
-        addresss_to_branch = 87
-        original_address = 99
-        cpu.handle_branch(addresss_to_branch)
-        self.assertEqual(cpu.program_counter, addresss_to_branch)
+        instruction = "040087"
+        memory.load_program([instruction])
+        address_to_branch = 87
+        await cpu.execute_instruction()
+        self.assertEqual(cpu.program_counter, address_to_branch)
 
-    def test_branch2(self):
+    async def test_branch2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
-        addresss_to_branch = 45
-        original_address = 34
-        cpu.handle_branch(addresss_to_branch)
-        self.assertEqual(cpu.program_counter, addresss_to_branch)
+        instruction = "040045"
+        memory.load_program([instruction])
+        address_to_branch = 45
+        await cpu.execute_instruction()
+        self.assertEqual(cpu.program_counter, address_to_branch)
 
-    def test_branch_neg1(self):
+    async def test_branch_neg1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "041045"
+        memory.load_program([instruction])
         cpu.accumulator.value = -5
-        addresss_to_branch = 45
-        original_address = 34
-        cpu.handle_branch_neg(addresss_to_branch)
-        self.assertEqual(cpu.program_counter, addresss_to_branch)
+        address_to_branch = 45
+        await cpu.execute_instruction()
+        self.assertEqual(cpu.program_counter, address_to_branch)
 
-    def test_branch_neg2(self):
+    async def test_branch_neg2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "041045"
+        memory.load_program([instruction])
         cpu.accumulator.value = 10
-        addresss_to_branch = 45
-        original_address = 34
-        cpu.handle_branch_neg(addresss_to_branch)
-        self.assertNotEqual(cpu.program_counter, addresss_to_branch)
+        address_to_branch = 45
+        await cpu.execute_instruction()
+        self.assertNotEqual(cpu.program_counter, address_to_branch)
 
-    def test_branch_zero1(self):
+    async def test_branch_zero1(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "042045"
+        memory.load_program([instruction])
         cpu.accumulator.value = 10
-        addresss_to_branch = 45
-        original_address = 34
-        cpu.handle_branch_zero(addresss_to_branch)
-        self.assertNotEqual(cpu.program_counter, addresss_to_branch)
+        address_to_branch = 45
+        await cpu.execute_instruction()
+        self.assertNotEqual(cpu.program_counter, address_to_branch)
 
-    def test_branch_zero2(self):
+    async def test_branch_zero2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "042056"
+        memory.load_program([instruction])
         cpu.accumulator.value = 0
-        addresss_to_branch = 56
-        original_address = 32
-        cpu.handle_branch_zero(addresss_to_branch)
-        self.assertEqual(cpu.program_counter, addresss_to_branch)
+        address_to_branch = 56
+        await cpu.execute_instruction()
+        self.assertEqual(cpu.program_counter, address_to_branch)
 
-    def test_halt1(self):
+    async def test_halt1(self):
         memory_size = 250
         memory = Memory(memory_size)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "043000"
+        memory.load_program([instruction])
+        await cpu.execute_instruction()
+        self.assertEqual(cpu.program_counter, memory_size)
+
+    async def test_halt2(self):
+        memory_size = 250
+        memory = Memory(memory_size)
+        input_handler = CLIInputHandler()
+        cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "043203"
+        memory.load_program([instruction])
         cpu.handle_halt()
         self.assertEqual(cpu.program_counter, memory_size)
 
-    def test_halt2(self):
+    async def test_invalid_instruction(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
-        cpu.handle_halt()
-        cpu.program_counter = 100
-
-    async def test_invalid_instrction(self):
-        memory = Memory(250)
-        input_handler = CLIInputHandler()
-        cpu = CPU(memory, input_handler, output_callback=print)
-        captured_word = io.StringIO()
         instruction = 8657
         cpu.memory.memory[0] = instruction
-        with patch('sys.stdout', new=captured_word):
+        with self.assertRaises(Exception) as context:
             await cpu.execute_instruction()
-        printed_value = captured_word.getvalue()
-        self.assertEqual("Invalid Instruction, please edit", printed_value.strip())
+        self.assertEqual(str(context.exception), "Invalid Instruction, please edit")
 
-    async def test_invalid_instrction2(self):
+    async def test_invalid_instruction2(self):
         memory = Memory(250)
         input_handler = CLIInputHandler()
         cpu = CPU(memory, input_handler, output_callback=print)
-        captured_word = io.StringIO()
         instruction = -8557
         cpu.memory.memory[0] = instruction
-        with patch('sys.stdout', new=captured_word):
+        with self.assertRaises(Exception) as context:
             await cpu.execute_instruction()
-        printed_value = captured_word.getvalue()
-        self.assertEqual("Invalid Instruction, please edit", printed_value.strip())
+        self.assertEqual(str(context.exception), "Invalid Instruction, please edit")
 
-
-if __name__ == "__main__":
-    unittest.main()
+    async def test_invalid_memory_address(self):
+        memory = Memory(250)
+        input_handler = CLIInputHandler()
+        cpu = CPU(memory, input_handler, output_callback=print)
+        instruction = "030256"
+        address = 256
+        memory.load_program([instruction])
+        with self.assertRaises(ValueError) as context:
+            await cpu.execute_instruction()
+        self.assertEqual(str(context.exception), f"Invalid address '{address}'; expected an address space less than 250")
